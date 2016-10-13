645e0e15abe9510aac715c186abdef738b7429aa
Thomas Gilgenast <thomasgilgenast@gmail.com>
2016-10-13 19:13:44 -0400

    first test of githubhook for automatic updating

---
+++ updater.js
@@ -0,0 +1,19 @@
+var exec = require('child_process').exec;
+var githubhook = require('githubhook');
+var github = githubhook({
+    'port': 1337,
+    'path': '/webhook',
+    'secret': process.env.SCOOTBOT_WEBHOOK_SECRET
+});
+
+github.listen();
+
+github.on('push:scootbot2.0:master', function (event, repo, ref, data) {
+    console.log('updating');
+    exec('git pull', function(error, stdout, stderr) {
+        console.log('restarting');
+        exec('forever restart app.js', function(error, stdout, stderr) {
+            console.log('restarted');
+        })
+    });
+});
